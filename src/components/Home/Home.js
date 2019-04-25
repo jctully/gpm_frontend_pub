962e83e3b10a9ad44962104bad90ae7b4056fc07
Joseph Tully <tullyj2@wwu.edu>
2019-04-24 20:40:33 -0700

    added portal page, style for each page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,19 +8,20 @@ export default class Home extends Component {
     render () {
         return (
             
-            <div id="container">
-            <div className='background'></div>
-            <div className='panel-left'></div>
-            <div className='panel-right'></div>
-
-            
-                <h1 className='page-title'> Welcome to the Graduate Program Manager Application!</h1>
-                <a href="/info">
-                    <button className="button">Info</button>
-                </a>
-                <a href="/login">
-                    <button className="button">Login</button>
-                </a>
+            <div className='MainContainer'>
+            <div className='banner'></div>
+                <div className='ContentContainer'>
+                    <h1 className='page-title'> Welcome to the Graduate Program Manager Application!</h1>
+                    <a href="/info">
+                        <button className="button">Info</button>
+                    </a>
+                    <a href="/login">
+                        <button className="button">Login</button>
+                    </a>
+                    <a href="/portal">
+                        <button id='bypass' className="button">BYPASS</button>
+                    </a>
+                </div>
             </div>
         )
     }
