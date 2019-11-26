ef34466ba36fba0f62437386a513daa6a6b2c5e9
Artem Dukhnitskiy <dukhnia@wwu.edu>
2019-11-26 12:37:53 -0800

    comment out menu in /admin

--- src/components/Portal/Menu.js
+++ src/components/Portal/Menu.js
@@ -1,52 +1,65 @@
 import React, { Component } from 'react';
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBCollapse, MDBContainer,
-MDBHamburgerToggler } from 'mdbreact';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBCollapse,
+  MDBContainer,
+  MDBHamburgerToggler
+} from 'mdbreact';
 
 function loc(curr) {
-  if(curr === "http://localhost:3000/portal#!") {
-    return "Portal";
+  if (curr === 'http://localhost:3000/portal#!') {
+    return 'Portal';
   }
 }
 
 class NavbarPage extends Component {
-state = {
-  collapse1: false,
-  collapseID: ''
-}
+  state = {
+    collapse1: false,
+    collapseID: ''
+  };
 
-toggleCollapse = collapseID => () => {
-  this.setState(prevState => ({ collapseID: (prevState.collapseID !== collapseID ? collapseID : '') }));
-}
+  toggleCollapse = collapseID => () => {
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : ''
+    }));
+  };
 
-toggleSingleCollapse = collapseId => {
-  this.setState({
-    ...this.state,
-    [collapseId]: !this.state[collapseId]
-  });
-}
+  toggleSingleCollapse = collapseId => {
+    this.setState({
+      ...this.state,
+      [collapseId]: !this.state[collapseId]
+    });
+  };
 
-render() {
-  return (
-    <MDBContainer>
-      <MDBNavbar color="#d3531a" width="100%">
-        <MDBContainer>
-          <MDBNavbarBrand>
-            {window.location.pathname}
-          </MDBNavbarBrand>
-          <MDBHamburgerToggler color="#d3531a" id="hamburger1" onClick={()=> this.toggleSingleCollapse('collapse1')} />
-            <MDBCollapse isOpen={this.state.collapse1} navbar>
+  render() {
+    return (
+      <MDBContainer>
+        <MDBNavbar color='#d3531a' width='100%'>
+          <MDBContainer>
+            <MDBNavbarBrand>{window.location.pathname}</MDBNavbarBrand>
+            <MDBHamburgerToggler
+              color='#d3531a'
+              id='hamburger1'
+              onClick={() => this.toggleSingleCollapse('collapse1')}
+            />
+            <MDBCollapse isOpen={this.state.collapse} navbar>
               <MDBNavbarNav left>
                 <MDBNavItem>
-                  <MDBNavLink to="/student/5d94e801fb8539513126219a/portal">Student</MDBNavLink>
+                  <MDBNavLink to='/student/portal'>Student</MDBNavLink>
                 </MDBNavItem>
                 <MDBNavItem>
-                  <MDBNavLink to="/admin">Admin</MDBNavLink>
+                  <MDBNavLink to='/admin'>Admin</MDBNavLink>
                 </MDBNavItem>
               </MDBNavbarNav>
             </MDBCollapse>
-        </MDBContainer>
-      </MDBNavbar>
-    </MDBContainer>
+          </MDBContainer>
+        </MDBNavbar>
+      </MDBContainer>
     );
   }
 }
